54a20aa8855460e6defec500e8f40d3cb7618240
xpclove <xpclove@126.com>
2018-05-16 17:48:12 +0800

    .

---
+++ src/qblas/qblas/obj/qsharp/src/q_walk.g.cs
@@ -0,0 +1,50 @@
+#pragma warning disable 1591
+using System;
+using Microsoft.Quantum.Primitive;
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.MetaData.Attributes;
+
+[assembly: OperationDeclaration("qblas", "q_walk () : ()", new string[] { }, "X:\\git\\qblas\\src\\qblas\\qblas\\q_walk.qs", 130L, 7L, 5L)]
+#line hidden
+namespace qblas
+{
+    public class q_walk : Operation<QVoid, QVoid>
+    {
+        public q_walk(IOperationFactory m) : base(m)
+        {
+            this.Dependencies = new Type[] { };
+        }
+
+        public override Type[] Dependencies
+        {
+            get;
+        }
+
+        public override Func<QVoid, QVoid> Body
+        {
+            get => (_args) =>
+            {
+#line hidden
+                this.Factory.StartOperation("qblas.q_walk", OperationFunctor.Body, _args);
+                var __result__ = default(QVoid);
+                try
+                {
+#line hidden
+                    return __result__;
+                }
+                finally
+                {
+#line hidden
+                    this.Factory.EndOperation("qblas.q_walk", OperationFunctor.Body, __result__);
+                }
+            }
+
+            ;
+        }
+
+        public static System.Threading.Tasks.Task<QVoid> Run(IOperationFactory __m__)
+        {
+            return __m__.Run<q_walk, QVoid, QVoid>(QVoid.Instance);
+        }
+    }
+}
